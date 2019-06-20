3555703d839fb9335609ae1d19701a4fde52cc52
_21Sancho <49732725+21Sancho@users.noreply.github.com>
2019-06-20 18:45:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,29 @@
-/*
- * Discord Bot Builder Bot
- * Version 1.2.0
- * Robert Borghese
- */
+const Discord = require('discord.js');
 
-const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
+const client = new Discord.Client();
 
-if(!process.send) {
+ 
 
-Files.initStandalone();
+client.on('ready', () => {
 
-} else {
+    console.log('I am ready!');
 
-process.on('message', function(content) {
-	Files.initBotTest(content);
 });
 
-}
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.NTczMTkwNDc4MDU3NDM5MjQz.XQu4MA.SJjwKJIYENu9ZE2bV55SbhxEZjw);//where BOT_TOKEN is the token of our bot
